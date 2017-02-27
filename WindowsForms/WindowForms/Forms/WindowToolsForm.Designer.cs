2bb827c5b94f775dce27e0be1754cf58ad15a2d1
李晨 <lichen@fissoft.com>
2017-02-27 17:40:51 +0800

    Base

---
+++ WindowsForms/WindowForms/Forms/WindowToolsForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowForms
+{
+    partial class WindowToolsForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(119, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "选中窗体句柄:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "选中窗体标题:";
+            // 
+            // WindowToolsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "WindowToolsForm";
+            this.Text = "WindowTools";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsForms/WindowForms/Forms/WindowToolsForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+using Core;
+using Tools;
+
+namespace WindowForms
+{
+    public partial class WindowToolsForm : Form
+    {
+        private bool Dragging { get; set; }
+
+        private IntPtr HwndCurrent { get; set; }
+
+        private ArrayList Innerintptr { get; set; }
+
+        public WindowToolsForm()
+        {
+            InitializeComponent();
+            pictureBox1.Cursor = Cursors.Hand;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = Resources.Drag;
+            Innerintptr = new ArrayList { pictureBox1.Handle };
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            Dragging = true;
+            Cursor = new Cursor(new MemoryStream(Resources.Eye));
+            ((PictureBox)sender).Image = Resources.Drag2;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!Dragging) return;
+            var intPtr = WindowsApi.WindowFromPoint(MousePosition);
+            if (Innerintptr.Contains(intPtr))
+            {
+                intPtr = IntPtr.Zero;
+            }
+            if (intPtr == HwndCurrent) return;
+            ApiTools.DrawRectFrame(HwndCurrent);
+            ApiTools.DrawRectFrame(intPtr);
+            HwndCurrent = intPtr;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            var pictureBox = ((PictureBox)sender);
+            if (!Dragging) return;
+            Dragging = false;
+            Cursor = Cursors.Default;
+            if (HwndCurrent == IntPtr.Zero) return;
+            ApiTools.DrawRectFrame(HwndCurrent);
+            HwndCurrent = IntPtr.Zero;
+            pictureBox.Image = Resources.Drag;
+            WindowsApi.SendMessage(Handle, 514u, 0, 0);
+        }
+    }
+}
